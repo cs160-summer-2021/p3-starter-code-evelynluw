48fb72be56dc2f6008427b981c0873e5fe2e4eba
Evelyn Lu <evelynlu@allganize.io>
2021-07-12 21:06:45 -0700

    setup

--- coloring/urls.py
+++ coloring/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('demo', views.index, name='demo'),
-    path('new_interaction', views.index, name='new_interaction')
+    path('demo', views.demo, name='demo'),
+    path('new_interaction', views.new_interaction, name='new_interaction')
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
+
+def demo(request):
+    return render(request, 'coloring/demo.html')
+
+def new_interaction(request):
+    return render(request, 'coloring/new_interaction.html')
