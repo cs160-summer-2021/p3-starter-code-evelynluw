14d8c400f5d7501162ea8dd7be49fc1bf83369b3
Evelyn Lu <evelynlu@allganize.io>
2021-07-15 09:11:08 -0700

    background, navbar done, toolbar mostly down

--- coloring/urls.py
+++ coloring/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('demo', views.demo, name='demo'),
-    path('new_interaction', views.new_interaction, name='new_interaction')
+    path('new_interaction', views.new_interaction, name='new_interaction'),
+    path('choose_template', views.choose_template, name='choose_template')
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -6,5 +6,8 @@ def index(request):
 def demo(request):
     return render(request, 'coloring/demo.html')
 
+def choose_template(request):
+    return render(request, 'coloring/choose_template.html')
+
 def new_interaction(request):
     return render(request, 'coloring/new_interaction.html')
