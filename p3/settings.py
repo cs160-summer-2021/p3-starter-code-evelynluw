4549c3e0b84a0df01e48b7e87e95872840ef3f6d
Evelyn Lu <yunerlu@berkeley.edu>
2021-07-15 22:09:53 -0700

    eraser and delete working

--- p3/settings.py
+++ p3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dt)!z4cwnj8#4e&rr94$k1s6^^(t*%@$2$)jt*hs&7^at-%72$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.34']
+ALLOWED_HOSTS = ['192.168.1.34', 'localhost']
 
 
 # Application definition
